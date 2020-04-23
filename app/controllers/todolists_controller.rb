5948fe7083d3e5e25ca107a1ffdeaf3012f9d8d1
mwataru-pro <m1130wata@gmail.com>
2020-04-23 22:21:33 +0900

    [Finish] 3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -11,8 +11,33 @@ class TodolistsController < ApplicationController
 
   	  redirect_to '/top'
   end
-  private
 
+  def index
+  	   @lists = List.all
+  end
+
+  def show
+  	  @list = List.find(params[:id])
+  end
+
+  def create
+  	  list = List.new(list_params)
+  	  list.save
+
+  	  redirect_to todolist_path(list.id)
+  end
+
+  def edit
+  		@list = List.find(params[:id])
+  end
+
+  def update
+        list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
+  end
+
+  private
   def list_params
   	params.require(:list).permit(:title,:body)
   end

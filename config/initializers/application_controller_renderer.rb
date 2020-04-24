f05679d5f1a0a11f3b90a06ebc61b690418345b0
mwataru-pro <m1130wata@gmail.com>
2020-04-24 19:00:44 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cdcee760b9f6fe5c8767fb8fbbcc246275e087e14a730cde618c3299d52cbdf89ab0f3cba10ded9151ccc369a005a3d82e5755ec624e8199602489fbe674da3c'

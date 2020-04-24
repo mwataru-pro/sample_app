c1aba9e589dfdba236a9d2b04d189e821bb850cc
mwataru-pro <m1130wata@gmail.com>
2020-04-24 22:16:09 +0900

    [Finish] 3-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  get 'todolists/new'
+ get 'todolists/new'
  
  get 'top' => 'homes#top'
  
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 
  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
- 
+
+ delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
+
 end
